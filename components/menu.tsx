6895a232e691fc41659440d4fa7664dedde77faf
Adjei Anet Frederic Junior <adjeifrederic@gmail.com>
2025-06-07 23:21:59 +0100

    mise à jour menu site

--- components/menu.tsx
+++ components/menu.tsx
@@ -22,7 +22,9 @@ const MenuSite = ({ page, isPage }: MenuSiteProps) => {
       ))}
       <Link
         href={page == "alimentaire" ? "/detergent" : "/alimentaire"}
-        className="bg-red-800 py-2 px-4 rounded-full font-bold uppercase text-white"
+        className={`py-2 px-4 rounded-full font-bold uppercase text-white ${
+          isPage ? "bg-[#0074b2]" : "bg-red-800"
+        }`}
       >
         {page == "alimentaire" ? "Détergents" : "Alimentaires"}
       </Link>
